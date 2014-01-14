817c4b6b2a08bf85e6cdbb5428400a560f830259
Johnny Greco <jgreco@princeton.edu>
2014-01-13 21:08:23 -0500

    added run_N_plot.py instructions and header comment

--- MultiNest_test/scripts/run_N_plot.py
+++ MultiNest_test/scripts/run_N_plot.py
@@ -1,5 +1,38 @@
 #!/usr/bin/env python 
 
+# ********************************************
+#
+# This is a script that will run the program 
+# and plot the posterior pdf with matplotlib. 
+# It can also only plot if you don't want to 
+# run the full program.
+#
+# How to run:
+#  
+# The script takes two command-line arguments 
+# plus 1 optional argument. Run using 
+#
+# ./run_N_plot.py runtime_filename logL|post (0|1)
+#
+# where runtime_filename is the name of the run time
+# file and logL or post refers to the z-axis you want 
+# to plot. The optional argument is a boolean for 
+# running the full program or just plotting; it is 
+# set to run and plot by default.
+#
+# Example: 
+# To run the lighthouse problem and plot the 
+# posterior pdf, use
+#
+# ./run_N_plot.py lighthouse.txt post 1 
+#
+#  to just plot (assuming posterior_pdfs.dat has 
+#  the lighthouse pdf in it),
+#
+# ./run_N_plot.py lighthouse.txt post 0
+#
+# ********************************************
+
 import sys, os
 import subprocess
 import numpy as np
@@ -9,7 +42,7 @@ from pylab import meshgrid
 from mpl_toolkits.mplot3d import Axes3D
 
 if (len(sys.argv) == 1 or len(sys.argv) > 4):
-    print "usage: "+sys.argv[0]+" runtime_filename logL|post [run code? (0|1)]"
+    print "usage: "+sys.argv[0]+" runtime_filename logL|post [run full code? (0|1)]"
     exit(1)
 
 os.chdir('..')
@@ -20,7 +53,7 @@ if len(sys.argv) == 4:
     run = int(sys.argv[3])
 
 filename = sys.argv[1]
-
+ 
 if run:
     subprocess.call(['./multinest', filename])
 
@@ -29,7 +62,6 @@ filename = 'RunTimeFiles/'+filename
 print 'plotting likelihood...'
 
 datafile = 'posterior_pdfs.dat'
-
 
 theta_min, theta_max = np.loadtxt(filename, skiprows=6, usecols=(3,4), unpack=True)
 x, y, logL, post = np.loadtxt(datafile, unpack=True)
